0dfcc5b9df61dc48baa4bdf9f0132494f40cedbf
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-07-03 08:11:12 +0530

    Create Valid Triangle.cpp

---
+++ Flowchart/Valid Triangle.cpp
@@ -0,0 +1,19 @@
+                                                                            VALID TRIANGLE
+                                                                            **************
+
+/*
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a,b,c;  // sides of triangle
+	cin >> a >> b >> c; 
+	if(a+b > c  &&  b+c > a && c+a > b)
+	{
+		cout << "Valid Triangle";
+	}
+	else
+		cout << "Not Valid";
+}
+*/
