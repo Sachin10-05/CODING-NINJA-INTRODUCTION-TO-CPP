443be0640d00b6bd3fc925a145b86de44fec109b
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-07-04 00:34:14 +0530

    Create Sum of Evens.cpp

---
+++ Flowchart/Sum of Evens.cpp
@@ -0,0 +1,24 @@
+                                                                                  SUM OF EVENS
+                                                                                  ************
+
+/*
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int N;
+	cin >>  N;
+	int sum = 0;
+	for(int i=1;i<=N;i++)
+	{
+		if(i%2 == 0)
+			sum += i;
+	}
+	cout << sum;
+}
+*/
+
+
+
+
+
