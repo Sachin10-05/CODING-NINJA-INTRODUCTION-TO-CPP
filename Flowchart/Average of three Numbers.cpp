b294dd10dfcd8f5ff4125e7fc2d682474dbf4fbc
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-07-03 08:10:28 +0530

    Create Average of three Numbers.cpp

---
+++ Flowchart/Average of three Numbers.cpp
@@ -0,0 +1,17 @@
+                                                                        AVERAGE OF THREE NUMBERS
+                                                                        ************************
+
+/*
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int num1,num2,num3;
+	cin >> num1 >> num2 >> num3;
+	int sum = (num1+num2+num3);
+	double avg = (double)sum/3;
+	cout << avg;
+	return 0;
+}
+*/
