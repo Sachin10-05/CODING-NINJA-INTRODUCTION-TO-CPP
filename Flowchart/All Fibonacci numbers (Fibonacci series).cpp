67debd7f4bf9184fa5172b333e746cc369741e33
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-07-04 00:38:22 +0530

    Create All Fibonacci numbers (Fibonacci series).cpp

---
+++ Flowchart/All Fibonacci numbers (Fibonacci series).cpp
@@ -0,0 +1,26 @@
+                                                                           ALL FOBONACCI NUMBERS
+                                                                           *********************
+
+
+/*
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int N;
+	cin >> N;
+	
+	int first = 0;
+	int last = 1;
+	cout << first << " ";
+	int res;
+	
+	while(res<=N)
+	{
+		cout << last << " ";
+		res = first + last;
+		first = last;
+		last = res;
+	}
+}
+*/
