3633f5b3ce0fb09a57400a7836d0db5794a1e58f
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-07-09 08:18:01 +0530

    Create 5. Diamond of Stars.cpp

---
+++ Pattern - 2/5. Diamond of Stars.cpp
@@ -0,0 +1,71 @@
+//                                                                     DIAMOND OF STARS
+//                                                                     ****************
+
+/*
+Sample Input 1:
+5
+Sample Output 1:
+  *
+ ***
+*****
+ ***
+  *
+  
+  */
+
+
+
+//SOLLUTION:
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i = 1, j, k, rows;
+    char ch;
+     
+    cin >> rows;
+    int N = (rows/2) + 1;
+
+
+    while(i <= N)
+    {
+        j = 1;
+    	while( j <= N - i)
+		{
+            cout << " ";
+            j++;
+        }
+        k = 1;
+        while( k <= i * 2 - 1)
+        {
+            cout << "*";
+            k++;
+        }
+        cout << endl;
+        i++;
+    }	
+
+
+
+
+    i = N - 1;
+    while( i > 0)
+    {
+    	j = 1;
+    	while( j <= N - i)
+		{
+            cout << " ";
+            j++;
+        }
+        k = 1;
+        while( k <= i * 2 - 1)
+        {
+            cout << "*";
+            k++;
+        }
+        cout << endl;
+        i--;
+    }	
+}
