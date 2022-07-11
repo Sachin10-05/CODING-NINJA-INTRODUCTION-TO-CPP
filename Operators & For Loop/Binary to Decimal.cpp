cda1bc260c1f2e4a59a20b920f23508b3329fa27
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-07-11 12:15:16 +0530

    Create Binary to Decimal.cpp

---
+++ Operators & For Loop/Binary to Decimal.cpp
@@ -0,0 +1,45 @@
+/*
+Given a binary number as an integer N, convert it into decimal and print.
+Sample Input 1 :
+1100
+Sample Output 1 :
+12
+
+Sample Input 2 :
+111
+Sample Output 2 :
+7
+*/
+
+
+
+
+
+
+
+//Solution
+----------
+  
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int N;
+	cin >> N;
+	int res=0;
+	int i=0;
+	
+  while(N!=0)
+	{
+		int rem=N%10;
+		res = res+(rem*pow(2,i));
+		i++;
+		N=N/10;
+	}
+	cout << res;
+}
+
+
+
+
+
