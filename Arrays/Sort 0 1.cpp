669c28e8fc30ea1a414d47eca76c60d6c5986d2f
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-07-16 14:26:58 +0530

    Create Sort 0 1.cpp

---
+++ Arrays/Sort 0 1.cpp
@@ -0,0 +1,72 @@
+/*
+Problem
+-------
+You have been given an integer array/list(ARR) of size N that contains only integers, 0 and 1. Write a function to sort this array/list. 
+Think of a solution which scans the array/list only once and don't require use of an extra array/list.
+
+Sample Input 1:
+1
+7
+0 1 1 0 1 0 1
+Sample Output 1:
+0 0 0 1 1 1 1
+
+Sample Input 2:
+2
+8
+1 0 1 1 0 1 0 1
+5
+0 1 0 1 0
+Sample Output 2:
+0 0 0 1 1 1 1 1
+0 0 0 1 1 
+
+*/
+//*******************************************************************************************************************************************************************
+//Solution:
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void sortZeroesAndOne(int *arr,int size)
+{
+	int count = 0;
+    for(int i=0;i<size;i++)
+    {
+        if(arr[i] == 0)
+            count++;
+    }
+    
+    for(int i=0;i<count;i++)
+        arr[i] = 0;
+    for(int i=count;i<size;i++)
+        arr[i] = 1;
+}
+
+
+int main()
+{
+	int t;    // no of test cases
+	cin >> t;
+	while (t--)
+	{
+		int size;
+		cin >> size;
+		int *input = new int[size];
+		for (int i = 0; i < size; ++i)
+		{
+			cin >> input[i];
+		}
+		sortZeroesAndOne(input, size);
+		for (int i = 0; i < size; ++i)
+		{
+			cout << input[i] << " ";
+		}
+		cout << endl;
+		delete[] input;
+	}
+	return 0;
+}
+
+
