cb8d2e10fc1440abf1d8bb4a4b060fb96e1fc6de
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-07-21 21:30:24 +0530

    Create Print All Substrings.cpp

---
+++ Character Array & 2D Array/Print All Substrings.cpp
@@ -0,0 +1,100 @@
+/*
+Sample Input 1:
+abc
+Sample Output 1:
+a 
+ab 
+abc 
+b 
+bc 
+c 
+
+Sample Input 2:
+co
+Sample Output 2:
+c 
+co 
+o
+*/
+
+
+//********************************************************************************************************************************************************************
+//                                                                          SOLUTION:
+//USING STRING
+//************
+
+/*
+#include<bits/stdc++.h>
+using namespace std;
+void printSubstrings(string str)
+{
+	for(int i=0;i<str.length();i++)
+	{
+		for(int j=1;j<str.length()+1;j++)
+		{
+			if(i+j <= str.length())
+				cout << str.substr(i,j) << "\n";
+		}
+	}
+}
+
+int main()
+{
+	string str;
+	cin >> str;
+	printSubstrings(str);
+	return 0;
+}
+*/
+
+
+
+//******************************************************************************************************************************************************************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
