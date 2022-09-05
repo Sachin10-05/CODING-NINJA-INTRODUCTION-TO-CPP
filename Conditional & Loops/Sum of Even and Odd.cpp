420e57752a44b010fc2642ead21557809b6d6dc5
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-09-05 22:01:23 +0530

    Update Sum of Even and Odd.cpp

--- Conditional & Loops/Sum of Even and Odd.cpp
+++ Conditional & Loops/Sum of Even and Odd.cpp
@@ -26,26 +26,25 @@ Sample Output 2:
 using namespace std;
 int main()
 {
-	int N;
-	cin >> N;
+   int N;
+   cin >> N;
 	
-	int Even_sum = 0;
-	int Odd_sum = 0;
-	while(N!=0)
-	{
-		int rem = N%10;
-		if(rem%2==0)
-			Even_sum += rem;
-		else
-			Odd_sum += rem;
-		N=N/10;
-	}
-	cout << Even_sum << " " << Odd_sum;
+   int Even_sum = 0;
+   int Odd_sum = 0;
+   while(N!=0)
+   {
+      int rem = N%10;
+      if(rem%2==0)
+	 Even_sum += rem;
+      else
+         Odd_sum += rem;
+      N=N/10;
+    }
+    cout << Even_sum << " " << Odd_sum;
 }
 
 
-
-
+//*******************************************************************************************************************************************************************
 
 
 
