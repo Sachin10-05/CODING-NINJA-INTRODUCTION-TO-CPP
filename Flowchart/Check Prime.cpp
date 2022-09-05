51ce477c57c188dcc930015ba6b3c9ee196e2ac2
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-09-05 21:54:59 +0530

    Update Check Prime.cpp

--- Flowchart/Check Prime.cpp
+++ Flowchart/Check Prime.cpp
@@ -1,29 +1,45 @@
                                                                             CHECK PRIME
                                                                             ***********
-/*
+									    
+//Time Complexity: O(sqrt(N))
+									    
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-	int n;
-	cin >> n;
-	bool res = true;
-	if(n<=1)
-		cout << "Not Prime";
-	else
-	{
-		for(int i=2;i<n;i++)
-		{
-			if(n%i ==0)
-			{
-				res = false;
-				break;
-			}
-		}
-		if(res)
-			cout << "Yes Prime";
-		else
-			cout << "Not Prime";
-	}
+   int n;
+   cin >> n;
+   bool res = true;
+   if(n<=1)
+      cout << "Not Prime";
+   else
+   {
+      for(int i=2;i<=sqrt(n);i++)     ☑☑☑☑☑
+      {
+         if(n%i == 0)
+	 {
+	    res = false;
+	    break;
+	 }
+      }
+      if(res)
+         cout << "Yes Prime";
+      else
+	 cout << "Not Prime";
+    }
 }
-*/
+
+
+
+
+
+
+//******************************************************************************************************************************************************************
+										    
+//Time Complexity: O(log(log(N)))
+//This can be achieved using "SIEVE OF ERATOSTHENES"
+										    
+										    
+										    
+										    
+
