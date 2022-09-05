53d3bfc95ebd294318d9a36e8419a30c834e3ace
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-09-05 21:35:20 +0530

    Update All Primes.cpp

--- Flowchart/All Primes.cpp
+++ Flowchart/All Primes.cpp
@@ -1,29 +1,33 @@
                                                                         ALL PRIMES
                                                                         **********
+									
+//Time Complexity : **O(N^2)**
 
 /*
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-	int N;
-	cin >> N;
-	for(int i=2;i<=N;i++)
-	{
-		bool res = true;
-		for(int j=2;j<i;j++)
-		{
-			if(i%j == 0)
-			{
-				res = false;
-				break;
-			}
-		}
-		if(res)
-			cout << i << " ";
-	}
+   int N;
+   cin >> N;
+   for(int i=2;i<=N;i++)
+   {
+      bool res = true;
+      for(int j=2;j<i;j++)
+      {
+         if(i%j == 0)
+	 {
+	    res = false;
+	    break;
+	 }
+      }
+      
+      if(res)
+	cout << i << " ";
+    }
 }
 */
+
 
 
 
