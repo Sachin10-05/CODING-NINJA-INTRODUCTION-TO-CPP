3df2254d5df91b07cbedd7c50a2f923b7b67f72a
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-09-14 13:48:53 +0530

    All prime from 1 to N inclusive

--- Flowchart/All Primes.cpp
+++ Flowchart/All Primes.cpp
@@ -1,6 +1,40 @@
-                                                                        ALL PRIMES
-                                                                        **********
-									
+                                                                        ALL PRIMES (from 1 to N)
+                                                                        ************************
+		
+										
+//Time Complexity: O(N*sqrt(N)
+//****************************
+										
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int N;
+   cin >> N;
+   for(int i=2;i<=N;i++)
+   {
+      bool flag = true;
+      for(int j=2;j<=sqrt(i);j++)     ☑☑☑☑☑
+      {
+         if(i%j == 0)
+	 {
+	    flag = false;
+	    break;
+	 }
+      }
+      
+      if(flag)
+	cout << i << " ";
+    }
+}
+
+
+
+
+//*******************************************************************************************************************************************************************
+
+
+
 //Time Complexity : **O(N^2)**
 
 /*
@@ -12,17 +46,17 @@ int main()
    cin >> N;
    for(int i=2;i<=N;i++)
    {
-      bool res = true;
+      bool flag = true;
       for(int j=2;j<i;j++)     ☑☑☑☑☑
       {
          if(i%j == 0)
 	 {
-	    res = false;
+	    flag = false;
 	    break;
 	 }
       }
       
-      if(res)
+      if(flag)
 	cout << i << " ";
     }
 }
@@ -33,6 +67,7 @@ int main()
 										
 
 //******************************************************************************************************************************************************************
+
 
 
 // Time Complexity: O(N^2)									
@@ -45,17 +80,17 @@ int main()
    cin >> N;
    for(int i=2;i<=N;i++)
    {
-      bool res = true;
+      bool flag = true;
       for(int j=2;j<=i/2;j++)     ☑☑☑☑☑
       {
          if(i%j == 0)
 	 {
-	    res = false;
+	    flag = false;
 	    break;
 	 }
       }
       
-      if(res)
+      if(flag)
 	cout << i << " ";
     }
 }
@@ -67,41 +102,7 @@ int main()
 										
 										
 //******************************************************************************************************************************************************************
-										
-										
-//Time Complexity: O(N*sqrt(N) = O(N^3/2)
-//***************************************
-										
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-   int N;
-   cin >> N;
-   for(int i=2;i<=N;i++)
-   {
-      bool res = true;
-      for(int j=2;j<=sqrt(i);j++)     ☑☑☑☑☑
-      {
-         if(i%j == 0)
-	 {
-	    res = false;
-	    break;
-	 }
-      }
-      
-      if(res)
-	cout << i << " ";
-    }
-}
-
-
-
-
-
-
-//*****************************************************************************************************************************************************************
-
+																
 // Time Complexity: O(n*log(log(n)))
 //This can be done using "SIEVE OF ERATOSTHENES."
 
