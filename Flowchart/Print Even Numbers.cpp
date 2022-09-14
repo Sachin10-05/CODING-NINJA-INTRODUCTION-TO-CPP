71f39e7e0db67f79fe7dbba66d468acf8744f3a2
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-09-14 12:38:49 +0530

    Even number from 1 to N

--- Flowchart/Print Even Numbers.cpp
+++ Flowchart/Print Even Numbers.cpp
@@ -4,14 +4,15 @@
 /*
 #include<bits/stdc++.h>
 using namespace std;
+
 int main()
 {
-	int N;
-	cin >> N;
-	for(int i=1;i<=N;i++)
-	{
-		if(i%2 == 0)
-			cout << i << " ";
-	}
+   int N;
+   cin >> N;
+   for(int i=1;i<=N;i++)
+   {
+      if(i%2 == 0)
+      cout << i << " ";
+   }
 }
 */
