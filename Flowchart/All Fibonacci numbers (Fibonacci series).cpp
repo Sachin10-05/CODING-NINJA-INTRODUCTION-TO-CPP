c1704ca6afd32ceb4cff03aae73f0a1d52fe910a
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-09-14 15:18:05 +0530

    Fibonacci series upto N

--- Flowchart/All Fibonacci numbers (Fibonacci series).cpp
+++ Flowchart/All Fibonacci numbers (Fibonacci series).cpp
@@ -1,26 +1,61 @@
                                                                            ALL FOBONACCI NUMBERS
                                                                            *********************
 
+/*
+Problem:
+You are given a single non-negative integer, N. You need to print all numbers that:
+(i) occur in the range 0 to N (both inclusive)
+(ii) are a part of the fibonacci sequence
+*/
+//*******************************************************************************************************************************************************************
+
+using Iteration
+---------------
+
+//Time complexity: O(N)
+//space complexity: O(1)
 
 /*
 #include<bits/stdc++.h>
 using namespace std;
-int main()
+
+int main() 
 {
-	int N;
-	cin >> N;
-	
-	int first = 0;
-	int last = 1;
-	cout << first << " ";
-	int res;
-	
-	while(res<=N)
-	{
-		cout << last << " ";
-		res = first + last;
-		first = last;
-		last = res;
-	}
-}
+    //fibonacci series upto N
+    int N;
+    cin >> N;
+    
+    int prev = 0;
+    int curr = 1;
+    
+    cout << prev << " ";
+    int res = 0;
+    while(res <= N)
+    {
+        res = prev + curr;
+        cout << curr << " ";
+        prev = curr;
+        curr = res;
+    }
 */
+										   
+
+										   
+//*******************************************************************************************************************************************************************										   
+										   
+
+										   
+								
+										   
+										   
+										   
+										   
+										   
+										   
+										   
+										   
+										   
+										   
+										   
+										   
+										   
