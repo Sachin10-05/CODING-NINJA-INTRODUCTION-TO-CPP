01bb23c8e52efdc5147f8cb3a276e8c9b557dc9e
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-09-14 15:32:22 +0530

    check N is member of fibonacci series or not

--- Flowchart/Member of Fibonacci.cpp
+++ Flowchart/Member of Fibonacci.cpp
@@ -1,38 +1,57 @@
                                                                         MEMBERS OF FIBONACCI
                                                                         ********************
-
 /*
+problem:
+You are given a single non-negative integer, N. You need to find out whether N is a part of the fibonacci sequence.
+Print "Yes" if it is and "No" if it's not.
+*/
+
+//*******************************************************************************************************************************************************************
+
+soluiton
+--------
+
+
 #include<bits/stdc++.h>
 using namespace std;
-int main()
+
+int main() 
 {
-	int N;
-	cin >> N;
-	
-	int first = 0;
-	int last = 1;
-	int res;
-	bool ans = false;
-	
-	while(res<=N)
-	{	
-		if(last==N)
-		{
-			ans = true;
-			break;
-		}
-		
-		res = first + last;
-		first = last;
-		last = res;
-	}
-	
-	if(ans)
-		cout << "Yes";
-	else
-		cout << "No";
+    int N;
+    cin >> N;
+    
+    //base case
+    if(N==0 || N==1)
+        cout << "Yes";
+    else
+    {
+        int prev = 0;
+        int curr = 1;
+        int res = 0;
+        bool flag = false;
+        int i=1;
+        while(i <= N)
+        {
+            res = prev + curr;
+            if(res == N)
+            {
+                flag = true;
+                break;
+            }
+            prev = curr;
+            curr = res;
+            i++;
+        }
+        
+        if(flag)
+            cout << "Yes";
+        else
+            cout << "No";
+    }
+        
 }
-*/
+
+
 
 
 
