f9fea93f14169098c19cad7fde2593feb2b6214e
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-09-14 12:36:05 +0530

    Factorial (N!)

--- Flowchart/Find Product.cpp
+++ Flowchart/Find Product.cpp
@@ -1,30 +1,11 @@
-                                                                    FIND PRODUCT (FACTORIAL)
-                                                                    ************************
-
-Iterative aproch
-----------------
-
-/*
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-	int N;
-	cin >> N;
-	int res = 1;
-	for(int i=1;i<=N;i++)
-	{
-		res = res * i;
-	}
-	cout << res;
-}
-*/
-
-
-
+                                                                    FIND PRODUCT (N!)
+                                                                    *****************
 
 Recursive approach
 ------------------
+
+//Time Complexity:  O(N)
+//space complexity: O(1)
 
 /*
 #include<bits/stdc++.h>
@@ -32,18 +13,110 @@ using namespace std;
 
 int fact(int N)
 {
-	if(N==0)   //base case
-		return 1;
-	return N*fact(N-1);	
+   //base case
+   if(N==0 || N==1)   
+      return 1;
+   return N*fact(N-1);	
 }
 
 int main()
 {
-	int N;
-	cin >> N;
-	int res = fact(N);
-	cout << res;
+   int N;
+   cin >> N;
+   int res = fact(N);
+   cout << res;
 }
 */
 
+
+
+//******************************************************************************************************************************************************************4
+
+Iterative (using For Loop) 
+--------------------------
+
+//Time complexity:  O(N)
+//space complexity: O(1)
+									    
+/*
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int n;
+    cin >> n;
+    int res = 1;
+    for(int i=2;i<=n;i++)
+        res = res*i;
+    cout << res;
+    return 0;
+}
+*/
+
+
+//********************************************************************************************************************************************************************
+
+
+Iterative (using While Loop) 
+----------------------------
+
+//Time complexity:  O(N)
+//space complexity: O(1)
+
+/*
+#include<bits/stdc++>
+using namespace std;
+int main() 
+{
+    int n;
+    cin >> n;
+    int res = 1;
+    int i=1;
+    while(n!=0)
+    {
+        res = res*i;
+        n--;
+        i++;
+    }
+    cout << res;
+    return 0;
+}								    
+*/									    
+									    
+									    
+									    
+									    
+									    
+									    
+									    
+									    
+									    
+									    
+									    
+									    
+									    
+									    
+									    
+									    
+									    
+									    
+									    
+									    
+									    
+									    
+									    
+									    
+									    
+									    
+									    
+									    
+									    
+									    
+									    
+									    
+									    
+									    
+									    
+									    
 
