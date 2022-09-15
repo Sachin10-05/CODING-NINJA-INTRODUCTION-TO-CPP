4aedcf8a02ce116cd41e10fa041ec6c67624f849
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-09-15 10:35:30 +0530

    Update Number Pattern - 2.cpp

--- Conditional & Loops/Number Pattern - 2.cpp
+++ Conditional & Loops/Number Pattern - 2.cpp
@@ -22,22 +22,21 @@ Sample Output :
 using namespace std;
 int main()
 {
-	int N;
-	cin >> N;
-	
-	for(int i=1;i<=N;i++)
-	{
-		for(int j=N-i;j>=1;j--)
-		{
-			cout << " " << " ";
-		}
-		
-		for(int k=i;k<2*i;k++)
-		{
-			cout << k << " ";
-		}
-		cout << "\n";
-	}
+   int N;
+   cin >> N;
+   for(int i=1;i<=N;i++)
+   {
+      for(int j=N-i;j>=1;j--)
+      {
+         cout << " ";
+      }
+      
+      for(int k=i;k<2*i;k++)
+      {
+         cout << k;
+      }
+      cout << "\n";
+    }
 }
 
 
