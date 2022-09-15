4a70aae9991f5e73d21c1ed51cc31cc1b77ddfad
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-09-15 08:10:10 +0530

    MCQs on Variables, data types, operators, etc

---
+++ Getting Started/MCQs.cpp
@@ -0,0 +1,209 @@
+1) What will be the output of the following code ?
+  #include <iostream>
+  using namespace std;
+  int main()
+  {
+    cout << code ;
+  }
+
+  a) code
+  b) Error ✔
+  
+  
+  
+  
+  
+2) What will be the output of the following code ?
+  #include <iostream>
+  using namespace std;
+  int main()
+  {
+    cout << "career" << "labs";
+  }
+
+  a) careerlabs ✔
+  b) career labs
+  
+  
+  
+  
+  
+3) Which of the following data type stores longest decimal number ?
+   a) long
+   b) float
+   c) double ✔
+   d) short
+   
+   Explaination: " Out of all given options, only float and double can hold decimal numbers."
+     
+     
+     
+     
+     
+4) Compiler assigns a garbage value to an uninitialised local variable in C++ Programming ?
+   a) true ✔
+   b) false
+     
+     
+     
+     
+     
+5) What will be the output of following statements -
+  bool a = true;
+  cout << a;
+  
+  Ans:- 1
+    
+    
+    
+    
+  
+6) What is the output of the following code if the input is : 5 15?
+   #include <iostream>
+   using namespace std;
+   int main() 
+   {
+      int a, b; 
+      cin >> a;
+      cin >> b;
+      cout << (a+b);
+      return 0;
+   }
+
+   Ans:- 20
+     
+     
+     
+     
+     
+7) What is the output of the following code if the input is : 2 10.1 D ?
+  #include <iostream>
+  using namespace std;
+  int main() 
+  {
+    int a;
+    double d;
+    char c;
+    cin >> a >> d >> c;
+    cout << a << d << c << endl;
+  }  
+
+  Ans:- 210.1D
+     
+
+    
+    
+    
+8) What will be the output of following statement ?
+  cout << (‘a’ + 1); 
+  
+  Ans:- 98
+    
+    
+    
+    
+    
+9) What will be the output of following statement ?
+  int i = 'c';
+  cout << i;   
+ 
+  Ans:- 99
+    
+    
+    
+    
+    
+10) What is the output ?
+  #include <iostream>
+  using namespace std;
+  int main()
+  {
+     char c = 74;
+     cout << c;
+     return 0;
+  }
+
+  Ans:- J
+  
+  
+  
+  
+  
+11) What is the output ?
+  #include <iostream>
+  using namespace std;
+  int main()
+  {
+    int a = 10;
+    char ch = 'a';
+    ch = ch + a;
+    cout << ch << endl;
+  }
+
+  Ans:- k
+  
+  
+
+
+
+12) What will be the output ?
+  #include <iostream>
+  using namespace std;   
+  int main()
+  {
+    double a = 6 / 4;
+    int b  = 6 / 4;
+    double c = a + b;
+    cout << c;
+  }
+
+  Ans:- 2
+  Explaination: When 6 / 4 is performed, both the operands of / are int hence answer will be an int i.e. 1. 
+                Hence the value of both a and b is 1. Thus a + b will be 2. 
+                Note: In C++, if a double value doesn't contain any decimal value, only integer will be printed.
+                  
+                  
+   
+                  
+                  
+13) What is the output
+  #include <iostream>
+  using namespace std;  
+  int main()
+  {
+    double a = 55.5;
+    int b = 55;
+    a = a % 10;
+    b = b % 10;
+    cout << a << " "  << b;
+  }
+
+  Ans:- Syntax error 
+  Explaination: % can’t be used with double or float.
+    
+    
+    
+    
+    
+14) What is the output
+  #include <iostream>
+  using namespace std;
+  int main() 
+  {
+    int var1 = 5;
+    int var2 = 6;
+    cout << (var1 > var2);
+  }
+
+  Ans:- 0
+  Explaination: '>' operator gives a boolean answer. 
+    
+   
+    
+    
+    
+    
+    
+       
+    
+    
