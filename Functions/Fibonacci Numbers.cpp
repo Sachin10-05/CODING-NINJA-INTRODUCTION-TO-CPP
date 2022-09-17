1ed632d16f800d12a8e1b3a0afa4fd916299dfc8
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-09-17 09:17:36 +0530

    is N in Fibonacci series

--- Functions/Fibonacci Numbers.cpp
+++ Functions/Fibonacci Numbers.cpp
@@ -2,7 +2,7 @@
 Problem
 -------
 Given a number N, figure out if it is a member of fibonacci series or not. Return true if the number is member of fibonacci series else false.
-Fibonacci Series is defined by the recurrence
+Fibonacci Series is defined by the recurrence:
     F(n) = F(n-1) + F(n-2)
 where F(0) = 0 and F(1) = 1
 
@@ -21,63 +21,68 @@ false
 
 
 
-
-
-
-
-
+*************************************************************************************************************************************************************************
 
 //Solution
-
-
+***********
 
 #include<bits/stdc++.h>
 using namespace std;
 
-
-
 bool checkMember(int n)
 {
-	if(n==0)
+    int a = 0;
+    int b = 1;
+    
+    if (n==a || n==b) 
+	return true;
+    else
+    {
+	int c = a+b;
+        while(c<=n)
+        {
+            if(c == n) 
 		return true;
-	else
-  {
-    int first = 0;
-		int last = 1;
-		int sum;
-	
-		bool res = false;
-		for(int i=0;i<=n;i++)
-		{
-			sum = first + last;
-			if(sum == n)
-			{
-				res = true;
-				break;
-			}
-			first = last;
-			last = sum;
-		}
-		return res;
-	}
-}
+            a = b;
+            b = c;
+            c = a + b;
+        }
+        return false;
+    }
+ }
 
 
 int main()
 {
-  int n; 
-  cin >> n ;
-  if(checkMember(n))
-  {
-	cout << "true" << endl;
-  }
-  else
-  {
-    cout << "false" << endl;
-  }
+    int n; 
+    cin >> n ;
+    if(checkMember(n))
+    {
+        cout << "true" << endl;
+    }
+    else
+    {
+        cout << "false" << endl;
+    }
 }
 
+	
+
+******************************************************************************************************************************************************************************
 
 
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
