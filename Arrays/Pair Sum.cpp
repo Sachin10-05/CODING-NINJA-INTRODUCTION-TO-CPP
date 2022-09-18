3173020cf705dbd12d9f4a99e8fc6fb462ef6c73
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-09-18 08:37:02 +0530

    Pair sum

--- Arrays/Pair Sum.cpp
+++ Arrays/Pair Sum.cpp
@@ -2,6 +2,14 @@
 Problem:
 --------
 You have been given an integer array/list(ARR) and a number X. Find and return the total number of pairs in the array/list which sum to X.
+Note: Given array/list can contain duplicate elements.
+
+Constraints :
+1 <= t <= 10^2
+0 <= N <= 10^3
+0 <= X <= 10^9
+Time Limit: 1 sec
+
 
 Sample Input 1:
 1
@@ -32,63 +40,54 @@ For the second query, we have 2 pairs in total that sum up to 10. They are, (2, 
 
 
 
+**********************************************************************************************************************************************************************************
 
+//solution-1
+*************
 
-
-
-
-//Solution
-//--------
-
+//Time complexity:O(N^2)
 
 #include<bits/stdc++.h>
 using namespace std;
-
 int pairSum(int *arr,int size,int x)
 {
-	int count= 0;
-	for(int i=0;i<size-1;i++)
-	{
-		for(int j=i+1;j<size;j++)
-		{
-			if(arr[i]+arr[j] == x)
-				count++;	
-		}	
-	}
-	return count;	
+   int count= 0;
+   for(int i=0;i<size;i++)
+   {
+      for(int j=i+1;j<size;j++)
+      {
+         if(arr[i]+arr[j] == x)
+	 count++;	
+      }	
+   }
+   return count;	
 }
-
-
-
-
 
 
 int main()
 {
-	int t;    //no of test cases
-	cin >> t;
-  while (t--)
-	{
-		int size;
-    cin >> size;
-		int *input = new int[size];
-    for (int i = 0; i < size; i++)
-		{
-			cin >> input[i];
-		}
-    int x;
-    cin >> x;
-    cout << pairSum(input, size, x) << endl;
-    delete[] input;
-	}
-	return 0;
+   int t;
+   cin >> t;
+   while (t--)
+   {
+      int size;
+      int x;
+      cin >> size;
+      int *input = new int[size];
+      for (int i = 0; i < size; i++)
+      {
+         cin >> input[i];
+      }
+      cin >> x;
+      cout << pairSum(input, size, x) << endl;
+      delete[] input;
+   }
+   return 0;
 }
 
 
 
 
-
-
-
+*******************************************************************************************************************************************************************************
 
 
