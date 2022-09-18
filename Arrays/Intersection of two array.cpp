e76336c5039aa4ce98f015bfa7ea46c8dcb5991b
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-09-18 08:28:14 +0530

    Intersection of two array

--- Arrays/Intersection of two array.cpp
+++ Arrays/Intersection of two array.cpp
@@ -3,6 +3,17 @@ You have been given two integer arrays/list(ARR1 and ARR2) of size N and M, resp
 You need to print their intersection; An intersection for this problem can be defined when both the arrays/lists contain a particular
 value or to put it in other words, when there is a common value that exists in both the arrays/lists.
 
+Note :
+       * Input arrays/lists can contain duplicate elements.
+       * The intersection elements printed would be in the order they appear in the first array/list(ARR1)
+       
+Constraints :
+1 <= t <= 10^2
+0 <= N <= 10^3
+0 <= M <= 10^3
+Time Limit: 1 sec 
+
+
 Sample Input 1 :
 2
 6
@@ -34,78 +45,95 @@ with the first '2' of the second array. Similarly, the second '2' of the first a
 
 
 
+***************************************************************************************************************************************************************************
 
+//solution-1
+*************
 
-
-
-
-
-//Solution:
-//---------
-
+//Time complexity: O(N^2)
 
 #include<bits/stdc++.h>
 using namespace std;
 
-void intersection(int *arr1,int *arr2,int size1,int size2)
+void intersection(int *input1, int *input2, int size1, int size2)
 {
-	int arr3[1000] = {0};
-	for(int i=0;i<size1;i++)
-	{
-		for(int j=0;j<size2;j++)
-		{
-			if(arr1[i] == arr2[j]   &&   arr2[j] != arr3[j])
-			{
-				cout << arr1[i] << " ";	
-				arr3[j] = arr2[j];
-				break;
-			}
-			
-		}	
-	}	
+   for(int i=0;i<size1;i++)
+   {
+      for(int j=0;j<size2;j++)
+      {
+         if(input1[i] == input2[j])
+         {
+            cout << input1[i] << " ";
+            input2[j] = INT_MIN;
+            break;
+         }
+      }
+   }
 }
-
-
-
-
 
 
 int main()
 {
-	int t;  // no of test cases
-	cin >> t;
-	while (t--)
-	{
-		int size1, size2;
-		cin >> size1;
-		int *input1 = new int[size1];
-		for (int i = 0; i < size1; i++)
-		{
-			cin >> input1[i];
-		}
+   int t;
+   cin >> t;
+   while (t--)
+   {
+      int size1, size2;
+      cin >> size1;
+      int *input1 = new int[size1];
+      for (int i = 0; i < size1; i++)
+      {
+         cin >> input1[i];
+      }
 
-		cin >> size2;
-		int *input2 = new int[size2];
-		for (int i = 0; i < size2; i++)
-		{
-			cin >> input2[i];
-		}
-
-		intersection(input1, input2, size1, size2);
-		
-		delete[] input1;
-		delete[] input2;
-		
-		cout << endl;
-	}
-
-	return 0;
+      cin >> size2;
+      int *input2 = new int[size2];
+      for (int i = 0; i < size2; i++)
+      {
+         cin >> input2[i];
+      }
+      intersection(input1, input2, size1, size2);
+      delete[] input1;
+      delete[] input2;
+      cout << endl;
+   }
+   return 0;
 }
 
 
 
 
 
+
+********************************************************************************************************************************************************************************
+
+//solution-2
+*************
+
+//Time complexity: O(N^2)
+
+void intersection(int *arr1,int *arr2,int size1,int size2)
+{
+   int arr3[1000] = {0};
+   for(int i=0;i<size1;i++)
+   {
+      for(int j=0;j<size2;j++)
+      {
+         if(arr1[i] == arr2[j]   &&   arr2[j] != arr3[j])
+	 {
+	    cout << arr1[i] << " ";	
+	    arr3[j] = arr2[j];
+	    break;
+	 }
+      }	
+   }	
+}
+
+
+
+
+
+*******************************************************************************************************************************************************************************
 
 
 
