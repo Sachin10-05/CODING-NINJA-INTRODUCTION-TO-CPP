e497073d42eec1c66dfc65422918b4b050cd89d5
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-09-18 07:34:32 +0530

    Pyramid Number Pattern

--- Test-1/Pyramid Number Pattern.cpp
+++ Test-1/Pyramid Number Pattern.cpp
@@ -9,36 +9,35 @@ N=4
 
 
 
+**********************************************************************************************************************************************************************************
+
+
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-	int N;
-	cin >> N;
-	
-	for(int i=1;i<=N;i++)           //rows
-	{
-		for(int j=1;j<=N-i;j++)     //columns
-		{
-			cout << " " << " ";
-		}
-		
-		
-		int x = i;
-		for(int k=i;k>=1;k--)
-		{
-			cout << x << " ";
-			x--;	
-		}
-		
-		for(int l=2;l<=i;l++)
-		{
-			cout << l << " ";
-		}
-		cout << "\n";
-	}
+    int N;                             
+    cin>>N;
+    
+    for (int i=1; i<=N; i++)                 
+    {
+        for (int j=N; j>i; j--)              
+        {
+            cout << (" ");
+        }
+        
+        for (int j=i; j>=1; j--)
+        {
+            cout << j;
+        }
+        
+        for (int j=2; j<=i; j++)
+        {
+            cout << j;
+        }
+        cout<< endl;
+    }
 }
-
 
 
 
