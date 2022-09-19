0bbd2eb7401f1f18eedd0ba968c80cfc6aa128ab
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-09-19 11:15:00 +0530

    push zeros to end

--- Searching and Sorting/Push Zeros to End.cpp
+++ Searching and Sorting/Push Zeros to End.cpp
@@ -4,6 +4,16 @@ Problem
 You have been given a random integer array/list(ARR) of size N. You have been required to push all the zeros that are present in the array/list to the end of it.
 Also, make sure to maintain the relative order of the non-zero elements.
 
+Note:
+    * Change in the input array/list itself. You don't need to return or print the elements.
+    * You need to do this in one scan of array only. Don't use extra space.
+    
+Constraints :
+1 <= t <= 10^2
+0 <= N <= 10^5
+Time Limit: 1 sec
+
+
 Sample Input 1:
 1
 7
@@ -13,70 +23,102 @@ Sample Output 1:
 Explanation for the Sample Input 1 :
 All the zeros have been pushed towards the end of the array/list. Another important fact is that the order of the non-zero elements have been 
 maintained as they appear in the input array/list.
-
-Sample Input 2:
-2
-5
-0 3 0 2 0
-5
-9 0 0 8 2
-Sample Output 2:
-3 2 0 0 0
-9 8 2 0 0 
-
 */
 
-//*******************************************************************************************************************************************************************
-//SOLUTION:
 
 
+
+
+//*************************************************************************************************************************************************************************
+
+
+//solution-1
+*************
+
+using two-pointer algorithm:-
+void pushZeroesEnd(int *arr, int n)
+{
+    int i = 0;
+    for(int j=0;j<n;j++)
+    {
+        if(arr[j] != 0)
+        {
+            swap(arr[j], arr[i]);
+            i++;
+        }
+    }
+}
+
+
+
+
+
+
+******************************************************************************************************************************************************************************
+
+
+
+//solution-2
+*************
 
 #include<bits/stdc++.h>
 using namespace std;
-
-void pushZeroesEnd(int *arr,int size)
+void pushZeroesEnd(int *input, int size)
 {
-	int k=0;
+    //Write your code here
+    int k=0;
 	for(int i=0;i<size;i++)
     {
-        if(arr[i] != 0)
+        if(input[i] != 0)
         {
-            arr[k] = arr[i];
+            input[k] = input[i];
             k++;
         }
     }
+    
     for(int i=k;i<size;i++)
-        arr[i] = 0;
+        input[i] = 0;
 }
-
 
 
 
 int main()
 {
-	int t;
-	cin >> t;
-	while(t--)
-	{
-		int size;
-		cin >> size;
-		
-		int *arr = new int[size];
-		for(int i=0;i<size;i++)
-		{
-			cin >> arr[i];
-		}
-		
-		pushZeroesEnd(arr,size);
-		for(int i=0;i<size;i++)
-		{
-			cout << arr[i] << " ";
-		}
-		cout << endl;
-		delete[] arr;
-	}
-	return 0;
+   int t;
+   cin >> t;
+   while (t--)
+   {
+      int size;
+      cin >> size;
+      int *input = new int[size];
+      for (int i = 0; i < size; i++)
+      {
+         cin >> input[i];
+      }
+      
+      pushZeroesEnd(input, size);
+
+      for (int i = 0; i < size; i++)
+      {
+         cout << input[i] << " ";
+      }
+      cout << endl;
+      delete[] input;
+   }
+   return 0;
 }
+
+
+
+
+
+
+****************************************************************************************************************************************************************************
+
+
+
+
+
 
 
 
