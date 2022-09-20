cb43043a2a2b15d3484c6d4761a018588eda775c
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-09-20 14:54:39 +0530

    String

--- Character Array & 2D Array/Compress the string.cpp
+++ Character Array & 2D Array/Compress the string.cpp
@@ -1,64 +1,86 @@
 /*
+problem:
+*********
+    	Write a program to do basic string compression. 
+    	For a character which is consecutively repeated more than once, replace consecutive duplicate occurrences with the count of repetitions.
+Constraints:
+	0 <= N <= 10^6
+	Where 'N' is the length of the input string.
+	Time Limit: 1 sec
+	
 Sample Input 1:
 aaabbccdsa
+
 Sample Output 1:
 a3b2c2dsa
-Explanation for Sample Output 1:
-In the given string 'a' is repeated 3 times, 'b' is repeated 2 times, 'c' is repeated 2 times and 'd', 's' and 'a' and occuring 1 time hence 
-no compression for last 3 characters.
-
-Sample Input 2:
-aaabbcddeeeee
-Sample Output 2:
-a3b2cd2e5
-Explanation for Sample Output 2:
-In the given string 'a' is repeated 3 times, 'b' is repeated 2 times, 'c' is occuring single time, 'd' is repeating 2 times and 'e' is repeating 5times.
 */
 
 
 
 
 
-//********************************************************************************************************************************************************************
+*********************************************************************************************************************************************************************************
 
-//                                                                      USING STRING
-//                                                                      ************
+
+* T.C = O(n)
+* S.C = O(n)
+*************
 
 #include<bits/stdc++.h>
 using namespace std;
-string getCompressedString(string str)
+
+string getCompressedString(string &str)
 {
-	int len = str.length();
-	string ans;
-	int i=0;
-	while(i<len)
-	{
-		int count=1;
-		while(str[i] == str[i+1])
-		{
-			count++;
-			i++;
-		}
-		if(count==1)
-			ans += str[i];
-		else
-			ans += str[i] + to_string(count);
-		
-		i++;
-	}
-	return ans;
+   int len = str.length();
+   string ans;
+   int i=0;
+   while(i<len)
+   {
+      int count=1;
+      while(str[i] == str[i+1])
+      {
+         count++;
+	 i++;
+      }
+      if(count==1)
+         ans += str[i];
+      else
+         ans += str[i] + to_string(count);
+      i++;
+   }
+   return ans;
 }
 
 
 
-int main()
+
+
+int main() 
 {
-	string str;
-	cin >> str;
-	
-	cout << getCompressedString(str);
-	return 0;
+   int size = 1e6;
+   string str;
+   cin >> str;
+   str = getCompressedString(str);
+   cout << str << endl;
 }
+
+
+
+
+
+
+*****************************************************************************************************************************************************************************
+
+
+
+
+
+
+
+
+
+
+
 
 
 
