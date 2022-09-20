62ba4c76cb2385ab9a83d73749250f76ae57c7ec
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-09-20 13:59:14 +0530

    Character Array

--- Character Array & 2D Array/2. Replace Character.cpp
+++ Character Array & 2D Array/2. Replace Character.cpp
@@ -2,53 +2,60 @@
 Problem:
 --------
 Given an input string S and two characters c1 and c2, you need to replace every occurrence of character c1 with character c2 in the given string.
-Input Format :
-Line 1 : Input String S
-Line 2 : Character c1 and c2 (separated by space)
 
-Output Format : Updated string
+Constraints :
+	1 <= Length of String S <= 10^6
 
 Sample Input :
 abacd
 a x
+
 Sample Output :
 xbxcd
 */
 
 
+
+
+
 //********************************************************************************************************************************************************************
 
+//solution:
+************
 
 
 #include<bits/stdc++.h>
 using namespace std;
-string replaceCharacter(string str,char c1,char c2)
+
+void replaceCharacter(char arr[], char c1, char c2) 
 {
-	for(int i=0;str[i] != '\0';i++)
-	{
-		if(str[i] == c1)
-		{
-			str[i] = c2;	
-		}	
-	}
-	return str;	
+    for(int i=0;input[i] != '\0';i++)
+    {
+        if(arr[i] == c1)
+            arr[i] = c2;
+    }
+}
+
+
+
+int main() 
+{
+    char input[1000000];
+    char c1, c2;
+    cin >> input;
+    cin >> c1 >> c2;
+    
+    replaceCharacter(input, c1, c2);
+    cout << input << endl;
 }
 
 
 
 
 
-int main()
-{
-	string s;
-	cin >> s;
-	
-	char c1,c2;
-	cin >> c1 >> c2;
-	
-	string res = replaceCharacter(s,c1,c2);
-	cout << res;
-}
+
+
+
 
 
 
